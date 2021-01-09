58197f481dea16b6f587313866a3213d0bd0e883
isobehiroki <h.sep23@gmail.com>
2021-01-09 13:02:55 +0900

    [Add]型の基本

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+    public static void main(String[] args) {
+    	long l = 10000000000L;
+    	System.out.println(l);
+    	double d = 3.14;
+    	System.out.println(d);
+    	boolean b = true; 
+    	System.out.println(b);
+    	
+    	Float f = 1.4142f;
+    	System.out.println(f);
+    	Integer i = 100;
+    	System.out.println(i);
+    	Character c = 'う';
+    	System.out.println(c);
+    	
+    	int[] array = {10, 20, 30, 40, 50};
+    	System.out.println(array[3]);
+    }
+}
