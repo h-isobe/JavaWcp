f50d6d1539c53fc957cf799aa4a47301f769a4f4
isobehiroki <h.sep23@gmail.com>
2021-01-24 20:45:56 +0900

    [Add]practice_exercise_3

--- src/practice_exercise/practice3.java
+++ src/practice_exercise/practice3.java
@@ -29,5 +29,89 @@ public class practice3 {
     	} else {
     		System.out.println("奇数です");
     	}
+    	
+    	//3-4 “正の偶数”、“正の奇数”、“負の偶数”、“負の奇数”
+    	int f = -5;
+    	if (f % 2 == 0) {
+    		if (f >= 0) {
+    			System.out.println("正の偶数");
+    		} else {
+    			System.out.println("負の偶数");
+    		}
+    	} else {
+    		if (f >= 0) {
+    			System.out.println("正の奇数");
+    		} else {
+    			System.out.println("負の奇数");
+    		}
+    	}
+    	
+    	//3-5 テストの採点
+    	//ケース1
+    	int score1 = 72;
+    	if (score1 >= 60) {
+    		System.out.println("合格");
+    	} else {
+    		System.out.println("不合格");
+    	}
+    	
+    	//ケース2
+    	int score2 = 79;
+    	if (score2 >= 80) {
+    		System.out.println("大変よくできました");
+    	} else if (score2 >= 60) {
+    		System.out.println("よくできました");
+    	} else {
+    		System.out.println("頑張りましょう");
+    	}
+    	
+    	//ケース3
+    	int score3 = 70;
+    	if (score3 >= 80) {
+    		System.out.println("優");
+    	} else if (score3 >= 70) {
+    		System.out.println("良");
+    	} else if (score3 >= 60) {
+    		System.out.println("可");
+    	} else {
+    		System.out.println("不可");
+    	}
+    	
+    	//3-6
+    	int test1 = 10;
+    	int test2 = 90;
+    	if (test1 >= 60 && test2 >= 60) {
+    		System.out.println("合格");
+    	} else if (test1 + test2 >= 130) {
+    		System.out.println("合格");
+    	} else if ((test1 + test2 >= 100) && (test1 >= 90 || test2 >= 90)) {
+    		System.out.println("合格");
+    	} else {
+    		System.out.println("不合格");
+    	}
+    	
+    	//3-7 病院の営業日
+    	//day  = {"0=日曜", "1=月曜", "2=火曜", "3=水曜", "4=木曜", "5=金曜", "6=土曜};
+    	//zone = {"0=午前", "1=午後", "2=夜間"};
+    	
+    	int day = 3;
+    	int zone = 1;
+    	boolean bool = true;
+    	
+    	if (day == 0) {
+    		bool = false;
+    	} else if (zone == 0 && (day == 2 || day == 5)) {
+    		bool = false;
+    	} else if (zone == 1 && (day == 6)) {
+    		bool = false;
+    	} else if (zone == 2 && (day == 3 || day == 6)) {
+    		bool = false;
+    	} 
+    	if (bool) {
+    		System.out.println("診療しています");
+    	} else {
+    		System.out.println("休診日です");
+    	}
+    	
     }
 }
