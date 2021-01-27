5afc722887bd92a6dba76acaf05b24974fb51226
isobehiroki <h.sep23@gmail.com>
2021-01-27 12:29:22 +0900

    [Update]pracitce_exercise_3

--- src/practice_exercise/practice3.java
+++ src/practice_exercise/practice3.java
@@ -127,5 +127,93 @@ public class practice3 {
     	} else {
     		System.out.println("xもyも負の数である、ではない。");
     	}
+    	
+    	//3-9
+    	System.out.println("何月ですか？");
+    	int month = 9;
+    	System.out.println(month + "月です");
+    	System.out.println(month + "月から年末までの祝日は");
+    	
+    	switch (month) {
+    		case 1:
+    			System.out.println("元日");
+    			System.out.println("成人の日");
+    		case 2:
+    		    System.out.println("建国記念の日");
+    		case 3:
+    			System.out.println("春分の日");
+    		case 4:
+    	        System.out.println("昭和の日");
+    	    case 5:
+    	        System.out.println("憲法記念日");
+    	        System.out.println("みどりの日");
+    	        System.out.println("こどもの日");
+    	    case 6:
+    	    case 7:
+    	        System.out.println("海の日");
+    	    case 8:
+    	    case 9:
+    	        System.out.println("敬老の日");
+    	        System.out.println("秋分の日");
+    	    case 10:
+    	        System.out.println("体育の日");
+    	    case 11:
+    	        System.out.println("文化の日" );
+    	        System.out.println("勤労感謝の日");
+    	    case 12:
+    	        System.out.println("天皇誕生日");
+    	}
+    	
+    	//3-10 鮨占い
+    	System.out.println("好きな寿司を選んでください。");
+    	System.out.println("1:まぐろ 2:えび 3: こはだ 4:あなご 5:いくら");
+    	int sushi = 3;
+    	switch (sushi) {
+    	    case 1:
+    	    	System.out.println("何か良いことがあります。");
+    	    	break;
+    	    case 2:
+    	    	System.out.println("驚くほど良いことがあります。");
+    	    	break;
+    	    case 3:
+    	    	System.out.println("ちょっと良いことがあります。");
+    	    	break;
+    	    case 4:
+    	    	System.out.println("とても良いことがあります。");
+    	    	break;
+    	    case 5:
+    	    	System.out.println("そこそこ良いことがあります。");
+    	    	break;
+    	}
+    	
+    	
+    	//3-11
+    	System.out.println("月の日数を表示");
+    	int month_days = 13;
+        
+    	switch (month_days) {
+    	    case 2:
+    	    	System.out.println("28日です");
+    	    	break;
+    	    case 4:
+    	    case 6:
+    	    case 9:
+    	    case 11:
+    	    	System.out.println("30日です");
+    	    	break;
+    	    case 1:
+    	    case 3:
+    	    case 5:
+    	    case 7:
+    	    case 8:
+    	    case 10:
+    	    case 12:
+    	    	System.out.println("31日です");
+    	    	break;
+    	    default:
+    	    	System.out.println("入力が間違っています");
+    	    	break;
+    	}
+    	    	
     }
 }
