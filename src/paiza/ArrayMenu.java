53848592fa07f7006c672b85381e66f5ba655956
isobehiroki <h.sep23@gmail.com>
2021-02-01 18:04:57 +0900

    [Add]array_menu

---
+++ src/paiza/ArrayMenu.java
@@ -0,0 +1,30 @@
+package paiza;
+
+import java.util.Scanner;
+
+public class ArrayMenu {
+    public static void main(String[] args) {
+
+    	//STEP: 1 要素数の出力
+    	System.out.println("要素数の数");
+    	Scanner sc = new Scanner(System.in);
+        int array[] = new int[10];
+        System.out.println(array.length + "個");
+
+
+    	//STEP: 2 全要素の出力
+        System.out.println("全要素の出力");
+    	Scanner sc2 = new Scanner(System.in);
+        int array2[] = {5, 1, 3, 4, 5, 12, 6, 8, 1, 3};
+        for (int arrays: array2) {
+            System.out.println(arrays);
+        }
+
+
+        //STEP: 3 i 番目の出力
+        System.out.println("以下の数列の 4 番目の要素を出力");
+        System.out.println("5 1 3 4 5 12 6 8 1 3");
+        int array3[] = {5, 1, 3, 4, 5, 12, 6, 8, 1, 3};
+        System.out.println(array3[3]);
+    }
+}
