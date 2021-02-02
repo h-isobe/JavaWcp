0f494fff11bbfd469e9db7790581158459c5bae1
isobehiroki <h.sep23@gmail.com>
2021-02-02 20:29:21 +0900

    [Update]array_menu

--- src/paiza/ArrayMenu.java
+++ src/paiza/ArrayMenu.java
@@ -36,6 +36,8 @@ public class ArrayMenu {
 
 
         //STEP: 5 配列の入力 2
+        System.out.println("10個の整数を半角区切りで与える");
+        System.out.println("この数列の全要素を改行区切りで出力");
         Scanner sc5 = new Scanner(System.in);
         String line5 = sc5.nextLine();
         String arrays5[] = line5.split(" ");
@@ -46,11 +48,24 @@ public class ArrayMenu {
 
 
         //STEP: 6 配列の入力 3
+        System.out.println("1 行目に整数 N が与えられる");
+        System.out.println("2 行目に、N 個の整数 a_i が半角スペース区切りで与えられる");
+        System.out.println("数列の全要素を改行区切りで出力");
         Scanner sc6 = new Scanner(System.in);
         int num6 = sc6.nextInt();
         for (int i = 0; i < num6; i++) {
         	int answer6 = sc6.nextInt();
         	System.out.println(answer6);
         }
+
+
+        //STEP: 7 i番目の出力 1
+        System.out.println("数列 a の num7 番目の要素を出力");
+        System.out.println("数列 a (1 3 5 4 6 2 1 7 1 5)");
+        Scanner sc7 = new Scanner(System.in);
+        int array7[] = {1, 3, 5, 4, 6, 2, 1, 7, 1, 5};
+        int num7 = sc7.nextInt();
+        System.out.println(array7[num7-1]);
+
     }
 }
