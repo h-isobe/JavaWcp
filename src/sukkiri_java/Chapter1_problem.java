70865dda213282b97cfb6139ea4a2717d5e5e3dd
isobehiroki <h.sep23@gmail.com>
2021-02-03 12:12:40 +0900

    [Add]chapter_1

---
+++ src/sukkiri_java/Chapter1_problem.java
@@ -0,0 +1,18 @@
+package sukkiri_java;
+
+public class Chapter1_problem {
+    public static void main(String[] args) {
+    	//練習問題 1-2
+    	int a = 3;
+    	int b = 5;
+    	System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + a * b);
+
+
+    	//練習問題 1-3
+    	boolean bool = true;
+    	char cha = '駆';
+    	double dou = 3.14;
+    	long lo = 314159265853979L;
+    	String str = "ミナトの攻撃!敵に15ポイントのダメージを与えた。";
+    }
+}
