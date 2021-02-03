07b4c5b0af888a28f76bbe221adeb7e3474654bd
isobehiroki <h.sep23@gmail.com>
2021-02-03 17:20:04 +0900

    [Add]chapter_4

---
+++ src/sukkiri_java/Chapter4_problem.java
@@ -0,0 +1,49 @@
+package sukkiri_java;
+
+public class Chapter4_problem {
+	public static void main(String[] args) {
+
+		//コード 4-9 for文を使って配列を扱う
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+
+
+		//コード 4-10 点数管理プログラム (for文の利用)
+		int[] scores2 = {20, 30, 40, 50, 80};
+		int sum = 0;
+		for (int i = 0; i < scores2.length; i++) {
+			sum += scores[i];
+		}
+		int avg = sum / scores.length;
+		System.out.println("合計点:" + sum);
+		System.out.println("平均点:" + avg);
+
+
+		//コード 4-11 50点以上の科目の数を調べる
+		int[] scores3 = {20, 30, 40, 50, 80};
+		int count = 0;
+		for (int i = 0; i < scores3.length; i++) {
+			if (scores3[i] >= 50) {
+				count++;
+			}
+		}
+		System.out.println("50点以上の科目の数は" + count);
+
+
+		//コード 4-12 DNAの記号をランダムに表示する
+		int[] seq = new int[10];  //10個の要素を持つ配列を用意
+		for (int i = 0; i < seq.length; i++) {
+			//ランダムに生成した0~4の数値を10回分配列に格納
+			seq[i] = new java.util.Random().nextInt(4);
+
+		    //塩基記号を配列baseに用意
+		    char[] base = {'A', 'T', 'G', 'C'};
+
+		    //ランダム生成された0~3のseq[i]を配列baseの添字にして表示
+		    System.out.print(base[seq[i]] + " ");
+		}
+	}
+
+}
