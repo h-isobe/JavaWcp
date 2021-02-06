28e6493a0579b851bbcd4fa3604de31f8fb1c325
isobehiroki <h.sep23@gmail.com>
2021-02-06 14:08:19 +0900

    [Add]chapter_6

---
+++ src/sukkiri_java06_calcapp/logics/CalcLogic.java
@@ -0,0 +1,13 @@
+package sukkiri_java06_calcapp.logics;
+
+public class CalcLogic {
+	
+	//コード6-2 計算処理クラス
+    public static int tasu(int a, int b) {
+    	return (a + b);
+    }
+    
+    public static int hiku(int a, int b) {
+    	return (a - b);
+    }
+}
